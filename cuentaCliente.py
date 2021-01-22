b875b146ec3a30d6625a3d35c5be0ec54d0ad9c1
Ricky <ricardombaezleal@gmail.com>
2021-01-21 23:30:43 -0300

    Se sube dudas

---
+++ cuentaCliente.py
@@ -0,0 +1,11 @@
+class PruebaPoo:
+    saldoPesos=0
+
+
+    def __init__(self,saldoPesos):
+        self.saldoPesos=saldoPesos
+
+
+
+    def muestroSaldo(self):
+        return print(self.saldoPesos)         
---
+++ main.py
@@ -0,0 +1,13 @@
+import bd
+import cuentaCliente
+
+
+for cliente in bd.lista:
+                hagoPrueba=cuentaCliente.PruebaPoo(cliente[0])
+
+hagoPrueba.muestroSaldo()
+    
+
+
+
+
